27d85580f94d0ba29d452361514fd0bb0823391c
louis <louis61619@gmail.com>
2021-08-31 14:13:18 +0800

    ci(scripts): create a shell to upload github pages

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# 當發生錯誤時終止腳本運行
+set -e
+# 打包
+npm run build
+# 移動至到打包後的dist目錄
+cd dist
+git init
+git add -A
+git commit -m 'deploy'
+# 部署到 https://github.com/XXXX/XXXX.git 分支為 gh-pages
+git push -f https://github.com/louis61619/vue3-ts-cms.git master:gh-pages
+cd -
